41f57434ea900ffeac74711c9bf37496b555a715
Douglas Obara <douglasobara97@gmail.com>
2022-06-12 00:34:12 +0300

    commit tree 306d4ca441772a2a08ca4e71b29456330e8d8ceb

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// returns an array of thank you messages for each name provided to the function
+const writeCards = (passedNames, passedMmessage) => {
+  let newArrayMessages = [];
+  for (let i = 0; i < passedNames.length; i++) {
+    newArrayMessages.push(
+      `Thank you, ${passedNames[i]}, for the wonderful ${passedMmessage} gift!`
+    );
+  }
+  return newArrayMessages;
+};
+
+// invokes console.log once for each number, counting down from the number provided to zero
+// logs each number when counting down, starting from the number provided
+const countDown = (passedNumber) => {
+  for (let i = passedNumber; i > -1; i--) {
+    console.log(i);
+  }
+};
